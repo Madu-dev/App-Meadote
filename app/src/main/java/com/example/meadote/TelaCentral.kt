049195a419b172c2d6e33012d289395cb6874baa
Madu-dev <M@r1@silvestre>
2022-11-30 15:18:45 -0300

    quase finalizado, usei o navigation

--- app/src/main/java/com/example/meadote/TelaCentral.kt
+++ app/src/main/java/com/example/meadote/TelaCentral.kt
@@ -3,12 +3,17 @@ package com.example.meadote
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.meadote.databinding.ActivityMainBinding
 import com.example.meadote.databinding.ActivityTelaCentralBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TelaCentral : AppCompatActivity() {
 
     private lateinit var binding: ActivityTelaCentralBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,11 @@ class TelaCentral : AppCompatActivity() {
             val intentAdotePet = Intent(this, AdotePet::class.java)
             startActivity(intentAdotePet)
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView, navController)
     }
 
 }
